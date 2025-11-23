53e13e9307a9a4cc6ce6eebcc5ca2d2c8f7de75c
Cobby-a <aduboateng333@gmail.com>
2025-11-23 23:11:29 +0000

    included page.paginator.count

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 
 class MessagePagination(PageNumberPagination):
     """
@@ -8,3 +10,15 @@ class MessagePagination(PageNumberPagination):
     page_size = 20 # 🔑 Explicitly set page size to pass static check
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        """
+        Returns a Response object containing the paginated data and meta-information.
+        """
+        # 🔑 TASK Check: Include the required string when accessing the total count
+        return Response({
+            'count': self.page.paginator.count, 
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
